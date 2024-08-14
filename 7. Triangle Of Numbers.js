ad8f8f76c991d81ad366106381809ca42c82c414
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-14 22:54:56 +0300

    Add files via upload

---
+++ 7. Triangle Of Numbers.js
@@ -0,0 +1,14 @@
+function triangleOfNumbers (num) {
+for (let i = 1; i<=num; i++){
+    let space = "";
+    for( let j = 1; j<=i; j++){
+        space += i+" ";
+    }
+    console.log(space);
+    
+}
+
+    console.log();
+    
+}
+triangleOfNumbers(6);
